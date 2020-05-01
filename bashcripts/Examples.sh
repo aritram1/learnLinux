713273ca53ec13e5c91c34610f099b946088131a
aritram1 <aritram1@gmail.com>
2020-05-02 01:34:45 +0530

    Update Examples.sh

--- bashcripts/Examples.sh
+++ bashcripts/Examples.sh
@@ -1,1 +1,45 @@
 # All the problems are covered from here : https://linuxhint.com/30_bash_script_examples/
+
+# Solve these beginner problems in bash scripts
+
+# Hello World
+echo 'Hello World!' # pretty simple
+# Echo Command
+
+# Comments
+# This is a test comment
+
+# Multi-line comment
+'
+This is 
+a multi
+line 
+comment
+'
+
+# While Loop
+# For Loop
+# Get User Input
+# If statement
+# And Condition if statement
+# Or Condition if statement
+# Else if and else condition
+# Case Condition
+# Get Arguments from Command Line 
+# Get arguments from command line with names
+# Combine two strings in a variable
+# Get Substring of Strings
+# Add 2 numbers into a variable
+# Create a Function
+# Use Function Parameters
+# Pass Return Value from Script
+# Make directory
+# Make directory by checking existence
+# Read a file
+# Delete a File
+# Append to file
+# Test if File Exists
+# Send Email Example
+# Get Parse Current Date
+# Wait Command
+# Sleep Command
