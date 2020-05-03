8b736d9d006bcd87480da7b66cf506ea64a8a2d9
aritram1 <aritram1@gmail.com>
2020-05-03 22:30:55 +0530

    Update and rename basicCommands.sh to filemanipulation.sh

--- utilities/filemanipulation.sh
+++ utilities/filemanipulation.sh
@@ -18,13 +18,3 @@ cd ../learnLinux
 mv linux_file ./test #moving
 mv ./test/renamed_linux_file ./test/linux_file # renaming
 # -----------------------------------------------------------------------------
-# netstat : netstat provides all connections to the host. It is easy to use the flags where 
--a (all connections), 
--i (only incoming connections), 
--s (only sending connections)
-Additionally a second flag shows more specific results.
--at/-au : shows all tcp/udp connections 
--st/-su : shows only sending connections
--it/-iu : shows only incoming connections
-# -----------------------------------------------------------------------------
-
