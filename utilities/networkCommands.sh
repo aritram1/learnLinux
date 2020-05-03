a0feda8a2679be0cfdb35ad04ff6e82419c2e7bc
aritram1 <aritram1@gmail.com>
2020-05-03 22:32:54 +0530

    Update networkCommands.sh

--- utilities/networkCommands.sh
+++ utilities/networkCommands.sh
@@ -1,10 +1,16 @@
 # List of most used network relatd commands in Bash
 
 # netstat : netstat provides all connections to the host. It is easy to use the flags where 
--a (all connections), 
--i (only incoming connections), 
--s (only sending connections)
-Additionally a second flag shows more specific results.
--at/-au : shows all tcp/udp connections 
--st/-su : shows only sending connections
--it/-iu : shows only incoming connections
+# -a (all connections), 
+# -i (only incoming connections), 
+# -s (only sending connections)
+# Additionally a second flag shows more specific results.
+# -at/-au : shows all tcp/udp connections 
+# -st/-su : shows only sending connections
+# -it/-iu : shows only incoming connections
+netstat -a
+netstat -at
+netstat -au
+netstat -it
+netstat -su
+# -------------------------------------------------------------------------------------
